8e6571a16ebab1b5c1ccd8663618f7e6181ade6f
sayuo4 <sayuo7898@gmail.com>
2026-02-08 16:50:48 +0200

    Add a fade animation for diving cylinder and light
    - Change RESET animation for death anim in HUD

--- src/items/diving_cylinder/diving_cylinder.gd
+++ src/items/diving_cylinder/diving_cylinder.gd
@@ -3,10 +3,12 @@ extends Area2D
 
 @export var oxygen_increase_amount: float
 @export var oxygen_pause_time: float
+@export var fade_time: float
 
-var is_anim_playing: bool = false
+var fade: float: set = set_fade
 
 @onready var sprite: Sprite2D = $Sprite2D as Sprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D as CollisionShape2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer as AnimationPlayer
 
 func _ready() -> void:
@@ -22,4 +24,18 @@ func _on_body_entered(body: Node2D) -> void:
 		hud.oxygen += oxygen_increase_amount
 		hud.pause_oxygen_for(oxygen_pause_time)
 		
+		collision_shape.set_deferred("disabled", true)
+		var tween: Tween = create_tween()
+		tween.tween_property(self, "fade", 1.0, fade_time)
+		
+		await tween.finished
+		
 		queue_free()
+
+func set_fade(value: float) -> void:
+	fade = value
+	
+	var sprite_material: ShaderMaterial = sprite.material as ShaderMaterial
+	
+	if sprite_material and sprite_material.get_shader_parameter("fade") != null:
+		sprite_material.set_shader_parameter("fade", value)
