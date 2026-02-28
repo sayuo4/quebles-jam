9f5c1ce7c1c99e52900cab38d34238515ac8990a
sayuo4 <sayuo7898@gmail.com>
2026-03-01 01:45:45 +0200

    Make camera shake less randomized
    - Add multipliers in PlayerCamera to change values globally

--- src/player/camera/player_camera.gd
+++ src/player/camera/player_camera.gd
@@ -1,15 +1,36 @@
 class_name PlayerCamera
 extends Camera2D
 
+@export_group("Shake")
+@export var shake_strength_multiplier: float = 1.0
+@export var shake_fade_multiplier: float = 1.0
+@export_range(0.0, 1.0) var jitter_amount: float
+
+@export_group("Zoom")
+@export var zoom_fade_multiplier: float = 1.0
+
 var _shake_fade: float = 0.0
 var _shake_strength: float = 0.0
 
 var _zoom_fade: float = 0.0
 
-var default_zoom: Vector2
+var _default_zoom: Vector2
 
 func _ready() -> void:
-	default_zoom = zoom
+	_default_zoom = zoom
+
+func _process(delta: float) -> void:
+	var multiplied_shake_fade: float = _shake_fade * shake_fade_multiplier
+	_shake_strength = lerpf(_shake_strength, 0.0, multiplied_shake_fade * delta)
+	
+	var multiplied_shake_strength: float = _shake_strength * shake_strength_multiplier
+	offset = Vector2(
+			multiplied_shake_strength * _get_random_jitter(jitter_amount),
+			multiplied_shake_strength * _get_random_jitter(jitter_amount)
+	)
+	
+	var multiplied_zoom_fade: float = _zoom_fade * zoom_fade_multiplier
+	zoom = zoom.lerp(_default_zoom, multiplied_zoom_fade * delta)
 
 func apply_shake(strength: float, fade: float) -> void:
 	_shake_strength = strength
@@ -19,12 +40,8 @@ func apply_zoom(value: Vector2, fade: float) -> void:
 	zoom = value
 	_zoom_fade = fade
 
-func _process(delta: float) -> void:
-	_shake_strength = lerpf(_shake_strength, 0.0, _shake_fade * delta)
+func _get_random_jitter(amount: float) -> float:
+	var dir: float = 1.0 if randi() % 2 == 0 else -1.0
+	var jitter: float = 1.0 + randf_range(-amount, amount)
 	
-	offset = Vector2(
-			randf_range(-_shake_strength, _shake_strength),
-			randf_range(-_shake_strength, _shake_strength)
-	)
-	
-	zoom = zoom.lerp(default_zoom, _zoom_fade * delta)
+	return dir * jitter
